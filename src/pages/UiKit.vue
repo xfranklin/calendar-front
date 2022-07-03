ba7a566505d321f231171c96a3750f42d0959307
dfeleniuk <d.feleniuk@oooi.app>
2022-07-03 23:30:16 +0300

    [master] (feat): fontVariable

--- src/pages/UiKit.vue
+++ src/pages/UiKit.vue
@@ -5,20 +5,21 @@
       <h2 class="subtitle-1">Typography</h2>
       <hr />
     </div>
-    <div style="display: flex">
-      <div>
-        <h1 class="headline-1">Headline 1</h1>
-        <h2 class="headline-2">Headline 2</h2>
-        <h3 class="headline-3">Headline 3</h3>
-        <div class="subtitle-1">Subtitle 1</div>
-        <div class="subtitle-2">Subtitle 2</div>
-        <div>text</div>
-        <div class="small-text">small text</div>
-      </div>
-      <div style="margin-left: 4rem">
-        <h1 class="variable">Headline 1</h1>
-      </div>
-    </div>
+
+    <h1 id="h1-variable" ref="variableFont">ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 0123456789</h1>
+    <br />
+    <input v-model="variableWeight" type="range" min="200" max="800" />
+    {{ variableWeight }}
+
+    <br /><br />
+
+    <h1 class="headline-1">Headline 1</h1>
+    <h2 class="headline-2">Headline 2</h2>
+    <h3 class="headline-3">Headline 3</h3>
+    <div class="subtitle-1">Subtitle 1</div>
+    <div class="subtitle-2">Subtitle 2</div>
+    <div>text</div>
+    <div class="small-text">small text</div>
 
     <div class="divider">
       <h2 class="subtitle-1">Form</h2>
@@ -37,28 +38,32 @@
   </div>
 </template>
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
+
+const variableFont = ref(null);
+const variableWeight = ref(100);
 
 const switch_1 = ref(false);
 const switch_2 = ref(true);
 const switch_3 = ref(false);
 const switch_4 = ref(true);
+
 const darkMode = ref(false);
 
 const disableTransitions = () => {
   const css = document.createElement("style");
   css.setAttribute("id", "off-transition");
-  css.appendChild(
-    document.createTextNode(
-      `* {
-       -webkit-transition: none !important;
-       -moz-transition: none !important;
-       -o-transition: none !important;
-       -ms-transition: none !important;
-       transition: none !important;
-      }`
-    )
-  );
+  // css.appendChild(
+  //   document.createTextNode(
+  //     `* {
+  //      -webkit-transition: none !important;
+  //      -moz-transition: none !important;
+  //      -o-transition: none !important;
+  //      -ms-transition: none !important;
+  //      transition: none !important;
+  //     }`
+  //   )
+  // );
   document.head.appendChild(css);
 };
 
@@ -83,9 +88,16 @@ const changeMode = async (value) => {
   });
   enableTransitions();
 };
+
+watch(variableWeight, (weight) => {
+  variableFont.value.style.fontWeight = weight;
+});
 </script>
+
 <style lang="scss" scoped>
-.header {
+#h1-variable {
+  font-size: 2rem;
+  font-weight: 100;
 }
 
 .ui-kit {
