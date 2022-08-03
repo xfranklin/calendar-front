a9c1b175270046a45955835d55d68398ca3b5e9e
s.poddubnyy <s.poddubnyy@keepsolid.com>
2022-08-04 02:16:13 +0300

    [oooi-0000] (fix): conflicts with development

--- src/plugins/init.js
+++ src/plugins/init.js
@@ -1,6 +1,22 @@
 import { useSettingsStore } from "@/store/settings";
+
 export const initPlugin = {
   async install() {
+    const colorMode = window.localStorage.getItem("COLOR_MODE");
     const settings = useSettingsStore();
+    if (colorMode) {
+      settings.setColorMode(colorMode);
+      if (colorMode === "LIGHT") {
+        document.documentElement.removeAttribute("dark");
+      } else if (colorMode === "DARK") {
+        document.documentElement.setAttribute("dark", "");
+      }
+    } else {
+      if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        window.localStorage.setItem("COLOR_MODE", "DARK");
+        settings.setColorMode("DARK");
+        document.documentElement.setAttribute("dark", "");
+      }
+    }
   }
 };
