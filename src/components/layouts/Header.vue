8b5bab7d3768135ae18d24249b21205f54c0a5c3
dfeleniuk <d.feleniuk@oooi.app>
2022-10-09 12:46:57 +0300

    [development] (fix): logo height

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -4,9 +4,10 @@
       v-if="settings.isDarkMode"
       class="auth-layout__logo"
       src="/src/assets/images/logo-dark.svg?url"
+      height="40"
       alt="oooi logo"
     />
-    <img v-else class="auth-layout__logo" src="/src/assets/images/logo.svg?url" alt="oooi logo" />
+    <img v-else class="auth-layout__logo" src="/src/assets/images/logo.svg?url" height="40" alt="oooi logo" />
   </header>
 </template>
 <script setup>
--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -6,9 +6,10 @@
         v-if="settings.isDarkMode"
         class="auth-layout__logo"
         src="/src/assets/images/logo-dark.svg?url"
+        height="40"
         alt="oooi logo"
       />
-      <img v-else class="auth-layout__logo" src="/src/assets/images/logo.svg?url" alt="oooi logo" />
+      <img v-else class="auth-layout__logo" src="/src/assets/images/logo.svg?url" height="40" alt="oooi logo" />
       <slot></slot>
     </div>
   </div>
