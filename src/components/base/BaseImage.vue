a387fcbac942f10cc7a0311de98ff2523f7090de
dfeleniuk <d.feleniuk@oooi.app>
2022-10-09 11:03:27 +0300

    [development] (fix): test img path

--- src/components/base/BaseImage.vue
+++ src/components/base/BaseImage.vue
@@ -22,14 +22,19 @@ const props = defineProps({
 
 const settings = useSettingsStore();
 const imageSrc = computed(() => {
-  try {
-    const src = settings.isDarkMode ? `${props.src}-dark.${props.type}` : `${props.src}.${props.type}`;
-    console.log(src);
-    console.log(new URL(src, import.meta.url));
-    return new URL(src, import.meta.url);
-  } catch (e) {
-    console.log(e);
-  }
-  return "";
+  const src = settings.isDarkMode ? `${props.src}-dark.${props.type}` : `${props.src}.${props.type}`;
+  console.log("=>", src);
+  const modules = import.meta.globEager("/src/assets/images/**/*");
+  console.log(modules);
+  return modules[src].default;
+  // try {
+  //   const src = settings.isDarkMode ? `${props.src}-dark.${props.type}` : `${props.src}.${props.type}`;
+  //   console.log(src);
+  //   console.log(new URL(src, import.meta.url));
+  //   return new URL(src, import.meta.url);
+  // } catch (e) {
+  //   console.log(e);
+  // }
+  // return "";
 });
 </script>
